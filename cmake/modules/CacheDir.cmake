ecd3447f1a03565aa895ba4335cdd9e60de668f8
archie2x <archie2x@users.noreply.github.com>
2026-05-06 14:48:32 -0700

    CPM / CPM_CACHE

---
+++ cmake/modules/CacheDir.cmake
@@ -0,0 +1,17 @@
+# most systems use per-user cache locations. This attempts to set a
+# SYS_CACHE_DIR variable to an appropriate value.
+
+if(NOT DEFINED SYS_CACHE_DIR)
+    if(XDG_CACHE_HOME)
+        set(SYS_CACHE_DIR ${XDG_CACHE_HOME})
+    elseif(DEFINED ENV{XDG_CACHE_HOME})
+        set(SYS_CACHE_DIR "$ENV{XDG_CACHE_HOME}")
+    elseif(WIN32)
+        file(REAL_PATH ~/Cache SYS_CACHE_DIR EXPAND_TILDE)
+    elseif(${CMAKE_HOST_SYSTEM_NAME} MATCHES "Darwin")
+        set(SYS_CACHE_DIR $ENV{HOME}/Library/Caches)
+    else()
+        set(SYS_CACHE_DIR $ENV{HOME}/.cache)
+    endif()
+endif()
+message("SYS_CACHE_DIR=${SYS_CACHE_DIR}")
